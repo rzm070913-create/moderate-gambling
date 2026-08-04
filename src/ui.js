cf7a88d431ebca2e25fc2d72bbf5d4107281badf
rzm070913-create <rzm070913@gmail.com>
2026-08-05 02:20:38 +0800

    Update ui.js

--- src/ui.js
+++ src/ui.js
@@ -1,104 +1,109 @@
-export function openCasinoUI(data, character){
+export function openCasinoUI(
+    data,
+    character
+){
+
+
+    $("#mg-casino-window").remove();
+
 
 
     const html = `
 
-
-    <div id="mg-casino-window">
-
-
-        <div class="mg-header">
-
-            <span>
-            🎰 适度赌博，赌狗万岁
-            </span>
+<div id="mg-casino-window">
 
 
-            <button id="mg-close">
-
-            ×
-
-            </button>
+    <div class="mg-header">
 
 
+        <div>
+        🎰 适度赌博，赌狗万岁
         </div>
 
 
+        <button id="mg-close">
 
-        <hr>
+        ×
 
-
-
-        <p>
-        💰 筹码：
-        <span>
-        ${data.coins}
-        </span>
-        </p>
-
-
-
-        <p>
-        🤖 当前角色：
-        ${character.name}
-        </p>
-
-
-
-        <hr>
-
-
-
-        <h3>
-        🎮 游戏大厅
-        </h3>
-
-
-        <button>
-        🎰老虎机
         </button>
-
-
-        <button>
-        🎲骰子
-        </button>
-
-
-        <button>
-        ✊猜拳
-        </button>
-
-
-
-        <h3>
-        🎁 福利中心
-        </h3>
-
-
-        <button>
-        📅每日签到
-        </button>
-
-
-        <button>
-        🆘救济金
-        </button>
-
 
 
     </div>
 
 
 
-    `;
+    <hr>
+
+
+
+    <div>
+    💰 筹码：
+
+    ${data.coins}
+
+    </div>
+
+
+
+    <div>
+    🤖 当前对手：
+
+    ${character.name}
+
+    </div>
+
+
+
+    <hr>
+
+
+
+    <h3>
+    🎮 游戏大厅
+    </h3>
+
+
+    <button>
+    🎰老虎机
+    </button>
+
+
+    <button>
+    🎲骰子
+    </button>
+
+
+    <button>
+    ✊猜拳
+    </button>
+
+
+
+    <h3>
+    🎁 福利中心
+    </h3>
+
+
+    <button>
+    📅每日签到
+    </button>
+
+
+    <button>
+    🆘救济金
+    </button>
+
+
+
+</div>
+
+`;
 
 
 
     $("body").append(html);
 
 
-
-    //关闭按钮
 
     $("#mg-close").on(
         "click",
