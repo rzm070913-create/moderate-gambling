cdae71067bd9825b912c9569e5634b3d9cd70775
rzm070913-create <rzm070913@gmail.com>
2026-08-05 23:24:58 +0800

    Update slot-symbols.js

--- games/slot/slot-symbols.js
+++ games/slot/slot-symbols.js
@@ -1,1 +1,83 @@
+export const SLOT_SYMBOLS = {
 
+    peach:{
+        name:"桃子",
+        css:"peach",
+        weight:2
+    },
+
+    cat:{
+        name:"猫",
+        css:"cat",
+        weight:10
+    },
+
+    dog:{
+        name:"狗",
+        css:"dog",
+        weight:12
+    },
+
+    fish:{
+        name:"鱼",
+        css:"fish",
+        weight:14
+    },
+
+    bird:{
+        name:"鸟",
+        css:"bird",
+        weight:14
+    },
+
+    mouse:{
+        name:"鼠",
+        css:"mouse",
+        weight:16
+    },
+
+    rabbit:{
+        name:"兔",
+        css:"rabbit",
+        weight:16
+    },
+
+    panda:{
+        name:"熊猫",
+        css:"panda",
+        weight:8
+    },
+
+    fox:{
+        name:"狐狸",
+        css:"fox",
+        weight:8
+    },
+
+    star:{
+        name:"星星",
+        css:"star",
+        weight:4
+    }
+
+};
+
+
+export function randomSymbol(){
+
+    const list=[];
+
+    Object.values(SLOT_SYMBOLS)
+    .forEach(item=>{
+
+        for(let i=0;i<item.weight;i++)
+            list.push(item);
+
+    });
+
+
+    return list[
+        Math.floor(Math.random()*list.length)
+    ];
+
+}
