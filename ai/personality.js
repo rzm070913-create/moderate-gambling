e170f74d10903d58c5a683075278ac47afd6ab44
rzm070913-create <rzm070913@gmail.com>
2026-08-05 23:08:22 +0800

    Update personality.js

--- ai/personality.js
+++ ai/personality.js
@@ -1,1 +1,24 @@
+export function createGamblerProfile(world){
+
+
+return {
+
+
+aggression:
+world.risk || 30,
+
+
+bluff:
+world.gambler ? 70 : 20,
+
+
+fold:
+world.rich ? 20 : 50
+
+
+
+};
+
+
+}
 
