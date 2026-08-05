2697f64150cd2b4e2d43bbb8a0da8b8324e9976e
rzm070913-create <rzm070913@gmail.com>
2026-08-05 22:29:16 +0800

    Update player.js

--- core/player.js
+++ core/player.js
@@ -1,1 +1,85 @@
+export const playerData = {
 
+
+    name:"",
+
+    chips:10000,
+
+    avatar:"",
+
+
+};
+
+
+export function loadPlayer(){
+
+
+const save =
+localStorage.getItem(
+"silly_casino_player"
+);
+
+
+if(save){
+
+Object.assign(
+playerData,
+JSON.parse(save)
+);
+
+}
+
+
+}
+
+
+
+export function savePlayer(){
+
+
+localStorage.setItem(
+
+"silly_casino_player",
+
+JSON.stringify(playerData)
+
+);
+
+
+}
+
+
+
+export function addChips(num){
+
+
+playerData.chips += num;
+
+
+savePlayer();
+
+
+}
+
+
+
+export function spendChips(num){
+
+
+if(playerData.chips < num){
+
+return false;
+
+}
+
+
+playerData.chips -= num;
+
+
+savePlayer();
+
+
+return true;
+
+
+}
