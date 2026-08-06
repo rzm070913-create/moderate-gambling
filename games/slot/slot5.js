6acd1b70e3f5bc70251d714ecdd48326c32e83b8
rzm070913-create <rzm070913@gmail.com>
2026-08-06 16:29:53 +0800

    Create slot5.js

---
+++ games/slot/slot5.js
@@ -0,0 +1,31 @@
+export function open(root){
+
+root.innerHTML=`
+
+<div class="slot-machine-container">
+
+<div class="grid" id="reelsContainer">
+
+</div>
+
+</div>
+
+
+<div class="controls">
+
+<button class="slot-spin">
+开始转动
+</button>
+
+</div>
+
+<div class="slot-result">
+
+</div>
+
+`;
+
+
+// 把你的测试版 JS 放这里
+
+}
